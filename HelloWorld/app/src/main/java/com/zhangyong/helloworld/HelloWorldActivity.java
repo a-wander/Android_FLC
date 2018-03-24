338e07136224d1ecf97d4f0e737abfa82b0daa30
a-wander <zhy905692718@163.com>
2018-03-24 15:39:09 +0800

    学习日志的写法

--- HelloWorld/app/src/main/java/com/zhangyong/helloworld/HelloWorldActivity.java
+++ HelloWorld/app/src/main/java/com/zhangyong/helloworld/HelloWorldActivity.java
@@ -2,12 +2,16 @@ package com.zhangyong.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class HelloWorldActivity extends AppCompatActivity {
+    private static final String TAG = "HelloWorldActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hello_world_layout);
+        Log.d("HelloWorldActivity", "test");
+        Log.d(TAG, "onCreate: debug log");
     }
 }
