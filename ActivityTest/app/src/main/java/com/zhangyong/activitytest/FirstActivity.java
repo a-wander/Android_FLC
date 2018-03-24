300eba699282a68210f7a4f32d4001c030e18e8e
a-wander <zhy905692718@163.com>
2018-03-24 16:40:31 +0800

    增加菜单，并响应

--- ActivityTest/app/src/main/java/com/zhangyong/activitytest/FirstActivity.java
+++ ActivityTest/app/src/main/java/com/zhangyong/activitytest/FirstActivity.java
@@ -2,6 +2,8 @@ package com.zhangyong.activitytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -21,5 +23,26 @@ public class FirstActivity extends AppCompatActivity {
                 Toast.makeText(FirstActivity.this, "你真听话！", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(FirstActivity.this, "不要乱点", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(FirstActivity.this, "就知道你会乱点，没了吧！", Toast.LENGTH_SHORT).show();
+                finish();// 结束当前activity
+                break;
+            default:
+        }
+        return true;
     }
 }
