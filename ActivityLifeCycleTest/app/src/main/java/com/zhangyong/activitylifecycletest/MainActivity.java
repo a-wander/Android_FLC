5eded9075780254cd0cd8565e9223b3d27c63047
a-wander <zhy905692718@163.com>
2018-03-31 18:21:22 +0800

    增加生命周期函数

--- ActivityLifeCycleTest/app/src/main/java/com/zhangyong/activitylifecycletest/MainActivity.java
+++ ActivityLifeCycleTest/app/src/main/java/com/zhangyong/activitylifecycletest/MainActivity.java
@@ -1,13 +1,78 @@
 package com.zhangyong.activitylifecycletest;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState != null) {
+            String temp = savedInstanceState.getString("key");
+            Log.d(TAG, "onCreate: "+temp);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.start_normal_activity) {
+            Intent intent = new Intent(MainActivity.this, NormalActivity.class);
+            startActivity(intent);
+        }
+        else if (id == R.id.start_dialog_activity) {
+            Intent intent = new Intent(MainActivity.this, DialogActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart: ");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("key", "value");
     }
 }
