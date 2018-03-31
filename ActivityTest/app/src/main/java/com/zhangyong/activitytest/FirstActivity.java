057f0970094a51534ab8f0b2e80e6284c31179d9
a-wander <zhy905692718@163.com>
2018-03-31 15:37:50 +0800

    接收返回数据

--- ActivityTest/app/src/main/java/com/zhangyong/activitytest/FirstActivity.java
+++ ActivityTest/app/src/main/java/com/zhangyong/activitytest/FirstActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,6 +13,7 @@ import android.widget.Toast;
 
 public class FirstActivity extends AppCompatActivity {
 
+    private static final String TAG = "FirstActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +26,9 @@ public class FirstActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Toast.makeText(FirstActivity.this, "你真听话！", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
-                startActivity(intent);
+                intent.putExtra("key", "value");
+                //startActivity(intent);
+                startActivityForResult(intent, 1);
             }
         });
 
@@ -33,7 +37,7 @@ public class FirstActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent("com.zhangyong.activitytest.ACTION_START");
-                // 只能有一个action，但是可以有多个category,当为匹配到时会崩溃
+                // 只能有一个action，但是可以有多个category,当未匹配到时会崩溃
                 intent.addCategory("com.zhangyong.activitytest.MY_CATEGORY");
                 startActivity(intent);
             }
@@ -69,5 +73,18 @@ public class FirstActivity extends AppCompatActivity {
             default:
         }
         return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode){
+            case 1:
+                if (resultCode == RESULT_OK){
+                    String res = data.getStringExtra("key1");
+                    Log.d(TAG, "onActivityResult: " + res);
+                }
+                break;
+            default:
+        }
     }
 }
--- ActivityTest/app/src/main/java/com/zhangyong/activitytest/SecondActivity.java
+++ ActivityTest/app/src/main/java/com/zhangyong/activitytest/SecondActivity.java
@@ -1,13 +1,32 @@
 package com.zhangyong.activitytest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private static final String TAG = "SecondActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_layout);
+
+        Intent intent = getIntent();
+        String strValue = intent.getStringExtra("key");
+        Log.d(TAG, "onCreate: "+strValue);
+
+        intent.putExtra("key1", "value1");
+        setResult(RESULT_OK, intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        //Toast.makeText(SecondActivity.this, "不允许返回！", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(SecondActivity.this, FirstActivity.class);
+        startActivity(intent);
     }
 }
